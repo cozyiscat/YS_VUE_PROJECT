3047de19f18f1e801f188f313863bdc572da22fa
cozyiscat <yuiop7664@gmail.com>
2024-08-01 09:27:53 +0900

    8/1 다시공부

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,38 +1,25 @@
 <template>
-  <div class="test">
-    <p>테스트화면입니다.</p>
+  <div>
+    <!-- 데이터 불러오는 방법 -->
+    <p>연세 IT 미래교육원</p>
+    <!-- 머스태치 {{  }} -->
+    <p>{{ title }}</p>
+    <p v-text="title"></p>
+    <p v-html="title"></p>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'TestView',
-  components: {
-    
+export default{
+  name:'TestVeiw',
+  components:{},
+  data(){
+    return{
+      title:'연세 IT <b>미래</b>교육원',
+    }
   },
-  data() {
-    return {
-      
-    };
-  },
-  setup() {
-    
-  },
-  created() {
-    
-  },
-  mounted() {
-    
-  },
-  unmounted() {
-    
-  },
-  methods: {
-    
-  }
-};
+  methods:{},
+}
 </script>
-
 <style scoped>
-
+/* scoped는 다른파일x 같은파일 안에서만 적용 */
 </style>
